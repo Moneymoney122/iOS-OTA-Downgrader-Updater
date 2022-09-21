fa7316492ba8908979cdf613ab9d7db182dda731
James Chandler <jameschandler750@gmail.com>
2022-09-21 23:18:45 +0100

    Add files via upload

---
+++ iOS-OTA-DowngraderUpdater.sh
@@ -0,0 +1,17 @@
+sudo cp -r ~/iOS-OTA-Downgrader ~/iOD-Files-Backup
+
+sudo rm -rf ~/iOD-Files-Backup/resources
+
+sudo rm -rf ~/iOD-Files-Backup/restore.cmd
+
+sudo rm -rf ~/iOD-Files-Backup/restore.sh
+
+sudo rm -rf ~/iOD-Files-Backup/README.md
+
+sudo rm -rf ~/iOS-OTA-Downgrader
+
+git clone https://github.com/LukeZGD/iOS-OTA-Downgrader
+
+sudo cp -r ~/iOD-Files-Backup ~/iOS-OTA-Downgrader
+
+sudo rm -rf ~/iOD-Files-Backup
